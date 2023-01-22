0737dc19e472e720d47d4f3cefcb7961c7ff1192
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2023-01-22 11:20:36 -0500

    Work on scanner

--- lib/scanner/update_scanner.dart
+++ lib/scanner/update_scanner.dart
@@ -1,3 +1,20 @@
-class UpdateScanner {
+import 'dart:convert';
 
+import 'package:http/http.dart';
+
+import 'objects/ttr_file.dart';
+
+class UpdateScanner {
+  Future<void> startScanner() async {
+    final uri = Uri.parse(
+        'https://cdn.toontownrewritten.com/content/patchmanifest.txt');
+    final response = await get(uri);
+    // The JSON response is a dictionary
+    Map<String, dynamic> json =
+        jsonDecode(response.body) as Map<String, dynamic>;
+    // Each value in the dictionary is a TTRFile
+    final map = json.map((key, value) =>
+        MapEntry(key, TTRFile.fromJson(value as Map<String, dynamic>)));
+    // TODO: Compare to database of updates
+  }
 }
